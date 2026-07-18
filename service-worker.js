db054512ca8ab35ed128d12381d60c5ab1859e88
prakashgoudvemula <prakashgoudvemula@gmail.com>
2026-07-19 00:13:19 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const CACHE_NAME = "regatte-granthalayam-v1";
 const SHELL_FILES = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
